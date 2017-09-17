e70804fd0d5d5ce4f0951ee8ec45202b2a67c19c
Kazuki Hasegawa <hasegawa@khasegawa.net>
2017-09-18 07:28:42 +0900

    remove puma

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
 require 'active_support/time'
 require 'active_support'
 require 'sinatra'
-require 'puma'
 
-configure { set :server, :puma }
+set :bind, ENV.fetch('TIMEZONES_BIND') { '0.0.0.0' }
+set :port, ENV.fetch('TIMEZONES_PORT') { 3000 }.to_i
 
 get '/' do
   erb :index
